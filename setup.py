cff1432ce26691f290ab54c44fcce32e04fb41e6
Sjaaky <sjaaky@users.sourceforge.net>
2011-09-23 14:29:40 +0200

    prepare for e2openplugins

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+pkg = 'Extensions.NfsServer'
+setup (name = 'enigma2-plugin-extensions-nfsserver',
+       version = '0.1',
+       description = 'NFS Server',
+       package_dir = {pkg: 'plugin'},
+       packages = [pkg]
+      )
